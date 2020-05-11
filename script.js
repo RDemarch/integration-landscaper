bfe3f89a0f745e2a203efba6ef97b9983d95cabb
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-05-12 01:11:30 +0200

    modifying navbar and header style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* functions */
 $(window).scroll(function () {
-  if ($(window).scrollTop() > 200) {
+  if ($(window).scrollTop() > 170) {
     $('.navbar').css('background', '#333').css('transition', 'all 0.5s');
   } else {
     $('.navbar').css('background', 'transparent').css('transition', 'all 0.5s');
