f7ab66482bf0e3d67422aea31a0223468e5755c4
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-05-13 00:31:00 +0200

    modified header, about and services section. added js for scroll detection

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-/* functions */
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 170) {
-    $('.navbar').css('background', '#333').css('transition', 'all 0.5s');
-  } else {
-    $('.navbar').css('background', 'transparent').css('transition', 'all 0.5s');
-  }
-});
-
 /*
 
 vanilla js version
 
-window.onscroll = function() {
-  console.log('scrolling');
-};
-
 */
+
+/* variables */
+const about = document.getElementById("aboutSection");
+const aboutY = about.scrollHeight;
+
+const services = document.getElementById("serviceSection");
+const servicesY = services.scrollHeight;
+
+/* functions */
+
+
+/* event handler */
+window.addEventListener("scroll", function(e) {
+  currentPosition = window.scrollY;
+  console.log("Current position: " + currentPosition + "px");
+  if (currentPosition > 170) {
+    document.getElementById("navbarSection").style.background = "#333";
+    document.getElementById("navbarSection").style.transition = "all 0.5s";
+  } else if (currentPosition > aboutY) {
+    document.getElementById("aboutLink").style.color = "#6aaf08";
+  } else {
+    document.getElementById("navbarSection").style.background = "transparent";
+    document.getElementById("navbarSection").style.transition = "all 0.5s";
+  }
+});
