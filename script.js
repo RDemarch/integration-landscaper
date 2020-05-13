5210ab7c794668d84608ae94f5ed4ee0aef5e168
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-05-13 16:34:09 +0200

    scrollspy almost working; header, about and services section improved

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-/*
-
-vanilla js version
-
-*/
+/* vanilla js version */
 
 /* variables */
-const about = document.getElementById("aboutSection");
-const aboutY = about.scrollHeight;
-
-const services = document.getElementById("serviceSection");
-const servicesY = services.scrollHeight;
 
 /* functions */
 
@@ -17,12 +8,9 @@ const servicesY = services.scrollHeight;
 /* event handler */
 window.addEventListener("scroll", function(e) {
   currentPosition = window.scrollY;
-  console.log("Current position: " + currentPosition + "px");
   if (currentPosition > 170) {
     document.getElementById("navbarSection").style.background = "#333";
     document.getElementById("navbarSection").style.transition = "all 0.5s";
-  } else if (currentPosition > aboutY) {
-    document.getElementById("aboutLink").style.color = "#6aaf08";
   } else {
     document.getElementById("navbarSection").style.background = "transparent";
     document.getElementById("navbarSection").style.transition = "all 0.5s";
